828ad80d117f70dfff10e62134ebd457cd09130b
niiko <nicolas.rocagel@gmail.com>
2026-04-02 20:28:36 +0200

    🐛 Fix video component

--- src/components/VideoPlr/index.tsx
+++ src/components/VideoPlr/index.tsx
@@ -1,23 +1,8 @@
 'use client'
 import { VideoSchema } from '@/types/video/schema'
-import { CldVideoPlayer } from 'next-cloudinary'
-import 'next-cloudinary/dist/cld-video-player.css'
 
 const VideoPlr = ({ videoID }: { videoID: VideoSchema['vidId'] }) => {
-    return (
-        <CldVideoPlayer
-            autoplay
-            loop
-            muted
-            controls={false}
-            bigPlayButton={false}
-            src={videoID}
-            id={videoID}
-            height={500}
-            width={500}
-            className="w-screen"
-        />
-    )
+    return <video autoPlay loop muted src={videoID} className="w-full" />
 }
 
 export default VideoPlr
