153b08c16a01980895cdfb2a5b921dd3e4c31d61
niiko <nicolas.rocagel@gmail.com>
2026-05-03 18:39:40 +0200

    🎨 Improve carousel structure

--- src/components/Modal/ModalContainer/index.tsx
+++ src/components/Modal/ModalContainer/index.tsx
@@ -18,23 +18,34 @@ interface ModalGalleryProps extends Omit<DialogTriggerProps, 'children'> {
 }
 
 const ModalContainer = ({ images }: ModalGalleryProps) => {
-    const [initialImageIndex, setInitialImageIndex] = useState(0)
+    const [selectedIndex, setSelectedIndex] = useState<number | null>(null)
 
     return (
         <div className='grid grid-cols-2 gap-4 sm:grid-cols-3 md:grid-cols-4'>
             {images.map((image, index) => (
-                <DialogTrigger key={`${image.imageId}-${index}`}>
-                    <Button
-                        onPress={() => setInitialImageIndex(index)}
-                        className='relative aspect-square overflow-hidden rounded-lg'
-                    >
-                        <Image
-                            src={image.url}
-                            alt={image.imageId}
-                            fill
-                            className='object-cover'
-                        />
-                    </Button>
+                <Button
+                    key={`${image.imageId}-${index}`}
+                    onPress={() => setSelectedIndex(index)}
+                    className='relative aspect-square overflow-hidden rounded-lg'
+                >
+                    <Image
+                        src={image.url}
+                        alt={image.imageId}
+                        fill
+                        className='object-cover'
+                    />
+                </Button>
+            ))}
+
+            {selectedIndex !== null && (
+                <DialogTrigger
+                    isOpen
+                    onOpenChange={(open) => {
+                        if (!open) setSelectedIndex(null)
+                    }}
+                >
+                    {/* dummy trigger required by API */}
+                    <span />
 
                     <Modal
                         isDismissable
@@ -43,12 +54,12 @@ const ModalContainer = ({ images }: ModalGalleryProps) => {
                         <Dialog className='flex h-full w-full items-center justify-center p-0 outline-none'>
                             <ImagesCarousel
                                 imagesCarousel={images}
-                                initialImageIndex={index}
+                                initialImageIndex={selectedIndex}
                             />
                         </Dialog>
                     </Modal>
                 </DialogTrigger>
-            ))}
+            )}
         </div>
     )
 }
