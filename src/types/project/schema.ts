9fcc2e36f97e8f434d43feb95937e21b6efd81f3
niiko <nicolas.rocagel@gmail.com>
2026-05-17 11:44:40 +0200

    🏷️ Change Project type

--- src/types/project/schema.ts
+++ src/types/project/schema.ts
@@ -5,12 +5,12 @@ import { ImageMetadata, VideoMetadata } from './index'
 
 export const ProjectSchema = z.object({
     _id: z.instanceof(ObjectId),
-    title: z.string().min(1).optional(),
+    title: z.string().min(1),
     description: z.string().min(1).optional(),
     video: VideoMetadata.optional(),
     category: z.string().min(1),
-    images: ImageMetadata.array().optional(),
-    order: z.number().int().nonnegative().optional(),
+    images: ImageMetadata.array(),
+    order: z.number().int().nonnegative(),
 })
 
 export type ProjectSchema = z.infer<typeof ProjectSchema>
