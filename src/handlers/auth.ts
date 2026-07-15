ccff69bc7a0a652b3f4cae78012ad109569ce3c6
niiko <nicolas.rocagel@gmail.com>
2026-07-15 20:58:29 +0200

    🚑️ Allow trusthost

--- src/handlers/auth.ts
+++ src/handlers/auth.ts
@@ -4,6 +4,7 @@ import Google from 'next-auth/providers/google'
 const allowedEmails = process.env.AUTH_GOOGLE_ALLOWED_EMAILS?.split(',') ?? []
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
+    trustHost: true,
     secret: process.env.AUTH_SECRET,
     providers: [
         Google({
